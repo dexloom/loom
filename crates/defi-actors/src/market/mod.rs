50f7a9936060794d39235aaf1ba8830674dc84d9
dexloom <144015139+dexloom@users.noreply.github.com>
2024-09-28 11:15:41 +0200

    Revert "Read uni v2/v3 pools addresses for initialization from db. (#30)" (#31)
    This reverts commit 0b502ecedd374be5fd47fd043dd54fe26d9d681d.

--- crates/defi-actors/src/market/mod.rs
+++ crates/defi-actors/src/market/mod.rs
@@ -1,11 +1,9 @@
-pub use db_pool_loader_actor::DbPoolLoaderOneShotActor;
 pub use history_pool_actor::HistoryPoolLoaderActor;
 pub use new_pool_actor::NewPoolLoaderActor;
 pub use pool_loader::{fetch_and_add_pool_by_address, fetch_state_and_add_pool, get_protocol_by_factory};
 pub use protocol_pool_actor::ProtocolPoolLoaderActor;
 pub use required_pools_actor::RequiredPoolLoaderActor;
 
-mod db_pool_loader_actor;
 mod history_pool_actor;
 mod logs_parser;
 mod new_pool_actor;
--- crates/defi-pools/src/db_reader/mod.rs
+++ crates/defi-pools/src/db_reader/mod.rs
@@ -1,5 +1,3 @@
-pub use uniswapv2::UniswapV2DBReader;
 pub use uniswapv3::UniswapV3DBReader;
 
-mod uniswapv2;
 mod uniswapv3;
