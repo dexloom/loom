6b0eb551112e0f0e67fbd4dc3fa776c219d565a2
cakevm <cakevm@proton.me>
2024-10-22 09:23:16 +0200

    Remove reth adapter and enable new engine

--- bin/loom_exex/src/arguments.rs
+++ bin/loom_exex/src/arguments.rs
@@ -1,5 +1,11 @@
 use clap::{Parser, Subcommand};
 
+/// Triggers persistence when the number of canonical blocks in memory exceeds this threshold.
+pub const DEFAULT_PERSISTENCE_THRESHOLD: u64 = 2;
+
+/// How close to the canonical head we persist blocks.
+pub const DEFAULT_MEMORY_BLOCK_BUFFER_TARGET: u64 = 2;
+
 #[derive(Debug, Subcommand)]
 pub enum Command {
     Node(LoomArgsNode),
@@ -20,4 +26,13 @@ pub struct LoomArgsNode {}
 pub struct LoomArgs {
     #[arg(long, default_value = "config.toml")]
     pub loom_config: String,
+
+    // Original RETH CLI arguments
+    /// Configure persistence threshold for engine experimental.
+    #[arg(long = "engine.persistence-threshold", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
+    pub persistence_threshold: u64,
+
+    /// Configure the target number of blocks to keep in memory.
+    #[arg(long = "engine.memory-block-buffer-target", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
+    pub memory_block_buffer_target: u64,
 }
